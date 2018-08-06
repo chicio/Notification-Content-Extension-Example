30d1d05023d06cdd35fb9bcd6c2f9b064b77f211
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-08-06 22:28:38 +0200

    Added notification actions :sparkles:

--- RichPushNotification/ViewController.swift
+++ RichPushNotification/ViewController.swift
@@ -13,16 +13,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        //Notification actions
+        let acceptAction = UNNotificationAction(identifier: "ACCEPT_ACTION",
+                                                title: "Accept",
+                                                options: [.foreground, UNNotificationActionOptions.authenticationRequired])
+        let declineAction = UNNotificationAction(identifier: "DECLINE_ACTION",
+                                                 title: "Decline",
+                                                 options: [])
+        //Notification category
+        let notificationCategory = UNNotificationCategory(identifier: "NasaDailyPhoto",
+                                                           actions: [acceptAction, declineAction],
+                                                           intentIdentifiers: [],
+                                                           hiddenPreviewsBodyPlaceholder: "",
+                                                           options: .customDismissAction)
+        notificationCenter.setNotificationCategories([notificationCategory])
+        
+        //Notification content
         let notificationContent = UNMutableNotificationContent()
         notificationContent.title = "Your Nasa Daily Photo"
         notificationContent.body = "Long press to see you daily nasa photo"
         notificationContent.sound = UNNotificationSound.default()
         notificationContent.categoryIdentifier = "NasaDailyPhoto"
         
+        //Notification trigger
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        //Notification request
         let request = UNNotificationRequest(identifier: "NasaNotification", content: notificationContent, trigger: trigger)
         
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        notificationCenter.add(request, withCompletionHandler: nil)
     }
 }
 
